318f88a883d28aad0cb5ca47a7192e49af447c5f
TomiG06 <thomasgkountas@gmail.com>
2026-04-02 21:42:48 +0300

    remove generated files after testing is done

--- test.sh
+++ test.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+trap 'rm bftest/*.out; exit' INT TERM
+
 for f in bftest/*.b; do
     echo
     echo "---------------"
     echo "Compiling $f.."
     echo
     ./brainf -c $f
-    ./out
+    base="${f%.b}"
+    ./${base}.out
 done
+
+rm bftest/*.out
